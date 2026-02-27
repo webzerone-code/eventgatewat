d6d6e2c8900ea00d41b6a8d70fa7429952b09666
Mohamed Reda <web_zerone@hotmail.com>
2026-02-28 00:40:36 +0200

    using redis messaging between services

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Transport } from '@nestjs/microservices';
+import { ConfigType } from '@nestjs/config';
+import RedisConfig from './config/redis.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
-
+  const rediaConf = app.get<ConfigType<typeof RedisConfig>>(RedisConfig.KEY);
   app.connectMicroservice({
     transport: Transport.REDIS,
     options: {
-      host: process.env.REDIS_HOST, // Use your config here
-      port: Number(process.env.REDIS_PORT),
+      host: rediaConf.REDIS_HOST, // Use your config here
+      port: rediaConf.REDIS_PORT,
     },
   });
   await app.startAllMicroservices();
